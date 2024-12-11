5cafe57637c97a18cad45517d7c9b43f1eaf495b
Yunus Emre <palitorch24@gmail.com>
2024-12-12 02:03:39 +0300

    Update SendBulkEmail.php

--- src/Console/Commands/SendBulkEmail.php
+++ src/Console/Commands/SendBulkEmail.php
@@ -1,1 +1,29 @@
+<?php
+
+namespace yunusasuroglu\BulkEmailer\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use yunusasuroglu\BulkEmailer\Mail\BulkMailer;
+
+class SendBulkEmail extends Command
+{
+    protected $signature = 'bulk-email:send 
+                            {email : Alıcı e-posta adresi} 
+                            {subject : E-posta konusu} 
+                            {message : E-posta içeriği}';
+
+    protected $description = 'Belirli bir e-posta adresine toplu e-posta gönderir.';
+
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $subject = $this->argument('subject');
+        $message = $this->argument('message');
+
+        Mail::to($email)->send(new BulkMailer($subject, $message));
+
+        $this->info("E-posta {$email} adresine başarıyla gönderildi!");
+    }
+}
 
