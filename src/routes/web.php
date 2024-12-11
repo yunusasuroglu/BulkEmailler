5c3dcdc672c38632d0f18afe5aca778b00c35cc0
Yunus Emre <palitorch24@gmail.com>
2024-12-12 02:06:02 +0300

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,10 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+use yunusasuroglu\BulkEmailer\Mail\BulkMailer;
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/send-test-email', function () {
+    Mail::to('test@example.com')->send(new BulkMailer('Test Konu', 'Bu bir test e-postasıdır.'));
+    return 'Test e-postası gönderildi!';
+});
