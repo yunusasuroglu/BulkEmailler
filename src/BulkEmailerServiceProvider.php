b9a57a9ea0b42836f50c83538c427ac9f5b4d4cd
Yunus Emre <palitorch24@gmail.com>
2024-12-12 02:06:20 +0300

    Update BulkEmailerServiceProvider.php

--- src/BulkEmailerServiceProvider.php
+++ src/BulkEmailerServiceProvider.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace yunusasuroglu\BulkEmailer;
+
+use Illuminate\Support\ServiceProvider;
+
+class BulkEmailerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Yönlendirme, görünümler ve yayınlama işlemleri
+        $this->loadViewsFrom(__DIR__ . '/Views', 'bulk-emailer');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    
+        $this->publishes([
+            __DIR__ . '/Config/bulk-emailer.php' => config_path('bulk-emailer.php'),
+        ], 'bulk-emailer-config');
+    }
+    
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/Config/bulk-emailer.php', 'bulk-emailer');
+        $this->commands([
+            \yunusasuroglu\BulkEmailer\Console\Commands\SendBulkEmail::class,
+        ]);
+    }
+}
