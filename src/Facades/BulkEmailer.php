cc279f4bee4c061532061deac4d12c5028914229
Yunus Emre <palitorch24@gmail.com>
2024-12-12 02:04:14 +0300

    Update BulkEmailer.php

--- src/Facades/BulkEmailer.php
+++ src/Facades/BulkEmailer.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace yunusasuroglu\BulkEmailer\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class BulkEmailer extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'bulk-emailer';
+    }
+}
