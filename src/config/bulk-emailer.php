f2909e052418daa4f95d45e2800a563b949007c7
Yunus Emre <palitorch24@gmail.com>
2024-12-12 02:05:34 +0300

    Update bulk-emailer.php

--- src/config/bulk-emailer.php
+++ src/config/bulk-emailer.php
@@ -1,1 +1,7 @@
+<?php
 
+return [
+    'default_from_address' => env('MAIL_FROM_ADDRESS', 'default@example.com'),
+    'default_from_name' => env('MAIL_FROM_NAME', 'Default Sender'),
+    'queue_emails' => true,  // Kuyruğa al seçeneği
+];
