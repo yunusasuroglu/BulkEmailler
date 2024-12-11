a4a12687169a968bf9716dff4fbd3ca9bf40a591
Yunus Emre <palitorch24@gmail.com>
2024-12-12 02:04:43 +0300

    Update BulkMailer.php

--- src/Mail/BulkMailer.php
+++ src/Mail/BulkMailer.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace yunusasuroglu\BulkEmailer\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class BulkMailer extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subjectLine;
+    public $messageBody;
+
+    public function __construct($subject, $body)
+    {
+        $this->subjectLine = $subject;
+        $this->messageBody = $body;
+    }
+
+    public function build()
+    {
+        return $this->subject($this->subjectLine)->view('bulk-emailer::emails.bulkmailer');
+    }
+}
