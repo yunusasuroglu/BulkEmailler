f623b78f6ebff620a43e3cfb52e4bd86b82c4269
Yunus Emre <palitorch24@gmail.com>
2024-12-12 15:09:41 +0300

    Update SendBulkEmail.php

--- src/Console/Commands/SendBulkEmail.php
+++ src/Console/Commands/SendBulkEmail.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace YunusAsuroglu\BulkEmailler\Console\Commands;
+namespace YunusAsuroglu\BulkEmailer\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
