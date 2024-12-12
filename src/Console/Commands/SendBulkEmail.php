27bbe6de67de2f8df05d06928b62d43bcdfddc59
Yunus Emre <palitorch24@gmail.com>
2024-12-13 00:11:47 +0300

    Update SendBulkEmail.php

--- src/Console/Commands/SendBulkEmail.php
+++ src/Console/Commands/SendBulkEmail.php
@@ -4,26 +4,47 @@ namespace YunusAsuroglu\BulkEmailler\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
-use yunusasuroglu\BulkEmailler\Mail\BulkMailer;
+use YunusAsuroglu\BulkEmailler\Mail\BulkMailer;
 
 class SendBulkEmail extends Command
 {
     protected $signature = 'bulk-email:send 
-                            {email : Alıcı e-posta adresi} 
-                            {subject : E-posta konusu} 
-                            {message : E-posta içeriği}';
-
-    protected $description = 'Belirli bir e-posta adresine toplu e-posta gönderir.';
-
+                            {emails : Recipients' email addresses (separated by commas)} 
+                            {subject : Email subject} 
+                            {data : Email Content}';
     public function handle()
-    {
-        $email = $this->argument('email');
-        $subject = $this->argument('subject');
-        $message = $this->argument('message');
-
-        Mail::to($email)->send(new BulkMailer($subject, $message));
-
-        $this->info("E-posta {$email} adresine başarıyla gönderildi!");
+{
+    $emails = explode(',', $this->argument('emails'));
+    $subject = $this->argument('subject');
+    $data = $this->argument('data');
+    
+    $sentEmails = [];
+    $failedEmails = [];
+    
+    foreach ($emails as $email) {
+        try {
+            Mail::to(trim($email))->send(new BulkMailer($subject, $data));
+            $sentEmails[] = $email; // Başarıyla gönderilen e-posta
+        } catch (\Exception $e) {
+            $failedEmails[] = ['email' => $email, 'error' => $e->getMessage()]; // Error Message
+        }
     }
+    
+    if (count($sentEmails) > 0) {
+        $this->info('Successfully sent email addresses:');
+        foreach ($sentEmails as $sentEmail) {
+            $this->info($sentEmail);
+        }
+    }
+    
+    if (count($failedEmails) > 0) {
+        $this->error('Email addresses that are not sent:');
+        foreach ($failedEmails as $failedEmail) {
+            $this->error($failedEmail['email'] . ' - Hata: ' . $failedEmail['error']);
+        }
+    } else {
+        $this->info("All emails sent successfully!");
+    }
+}
 }
 
